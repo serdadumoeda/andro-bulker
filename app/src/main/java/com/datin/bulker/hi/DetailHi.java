e2cfb3a588afaaf57dcca43354f0c88cc3f00cbf
arif budi setiawan <arifbudi.setiawan@gmail.com>
2019-06-11 07:10:08 +0700

    update detail hiwas

--- app/src/main/java/com/datin/bulker/hi/DetailHi.java
+++ app/src/main/java/com/datin/bulker/hi/DetailHi.java
@@ -1,6 +1,7 @@
 package com.datin.bulker.hi;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ import com.datin.bulker.hi.HiItem;
 public class DetailHi extends AppCompatActivity {
 
     public static String EXTRA_PLAYER = "extra_player";
-    TextView tvJudul, tvDeskripsi;
+    TextView tvJudul, tvDeskripsi, tvTahun, tvTanggal;
 
 
 
@@ -20,17 +21,27 @@ public class DetailHi extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_hi);
 
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.judul);
+        collapsingToolbarLayout.setTitleEnabled(true);
+
         tvJudul = findViewById(R.id.tvJudul);
+        tvTahun = findViewById(R.id.tvTahun);
+        tvTanggal = findViewById(R.id.tvTanggal);
         tvDeskripsi = findViewById(R.id.tvDeskripsi);
 
 
         HiItem HiItem =  getIntent().getParcelableExtra(EXTRA_PLAYER);
 
         String judul = HiItem.getJudul();
+        String tahun = HiItem.getTh_id();
+        String tanggal = HiItem.getCreated_at();
         String deskripsi = HiItem.getDeskripsi();
 
         tvJudul.setText(judul);
+        collapsingToolbarLayout.setTitle(judul);
         tvDeskripsi.setText(Html.fromHtml(deskripsi));
+        tvTanggal.setText(tanggal);
+        tvTahun.setText(tahun);
 
 
     }
